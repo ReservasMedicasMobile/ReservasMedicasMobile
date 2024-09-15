776d08b8aa1acd24089868697782dfe104be8027
Monica Antich <91569663+MonicaAntich@users.noreply.github.com>
2024-09-14 23:19:14 -0300

    Alertas en cada boton

--- app/src/main/java/com/example/reservasmedicasmobile/MainActivity.java
+++ app/src/main/java/com/example/reservasmedicasmobile/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.reservasmedicasmobile;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.AlertDialog;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -44,30 +45,43 @@ public class MainActivity extends AppCompatActivity {
 
         // analiza el evento del item presionado y redirecciona activity
         if (id == R.id.Inicio) {
+            mostrarAlerta("Navegando a Inicio");
             // Navigate to Inicio
             return true;
         } else if (id == R.id.Contacto) {
+            mostrarAlerta("Vas a Contacto");
             // va a Contacto
             return true;
         } else if (id == R.id.Especialidad) {
+            mostrarAlerta("Vas a Especialidad");
             // va a Especialidad
             return true;
         } else if (id == R.id.Registro) {
+            mostrarAlerta("Vas a Registro");
             // va a Registro
             return true;
         } else if (id == R.id.Dashboard) {
+            mostrarAlerta("Vas a Dashboard");
             // va a Dashboard
             return true;
         } else if (id == R.id.Iniciar_Sesion) {
+            mostrarAlerta("Vas a Iniciar Sesión");
             // va a Iniciar Sesion
             return true;
         } else if (id == R.id.Cerrar_Sesion) {
+            mostrarAlerta("Vas a Cerrar Sesión");
             // va a Cerrar_Sesion
             return true;
         }
-
-
-
         return super.onOptionsItemSelected(item);
     }
+    private void mostrarAlerta(String mensaje) {
+        new AlertDialog.Builder(this)
+                .setTitle("Alerta")
+                .setMessage(mensaje)
+                .setPositiveButton("OK", null)
+                .show();
+    }
+
+
 }
