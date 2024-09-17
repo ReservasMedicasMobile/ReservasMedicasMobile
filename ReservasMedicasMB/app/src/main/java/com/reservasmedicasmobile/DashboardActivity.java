2332ebd492957f1414e5db7ea4cf86dd1822cdc4
Matias Sorrentino <sorrentino.matias@hotmail.com>
2024-09-17 08:48:44 -0300

    Subo Dashboard con cambios en las Dependencias y solución de errores

---
+++ ReservasMedicasMB/app/src/main/java/com/reservasmedicasmobile/DashboardActivity.java
@@ -0,0 +1,16 @@
+package com.reservasmedicasmobile;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        // Puedes dejar el método vacío si no necesitas lógica adicional.
+    }
+}
+
