cc35be464226c17f0cf633badc07e871772c287f
Daniel Alejandro Segall <131227799+pupaycleo@users.noreply.github.com>
2024-09-18 18:23:02 -0300

    Activity contacto
    Cambios

--- app/src/main/java/com/example/reservasmedicasmobile/contacto.java
+++ app/src/main/java/com/example/reservasmedicasmobile/contacto.java
@@ -1,6 +1,9 @@
 package com.example.reservasmedicasmobile;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class contacto extends AppCompatActivity {
 
+    private EditText etFirstName, etLastName, etEmail, etPhoneNumber, etMessage;
+    private Button btnSubmit, btnBack;
+    private TextView tvData;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_contacto);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        etFirstName = findViewById(R.id.etFirstName);
+        etLastName = findViewById(R.id.etLastName);
+        etEmail = findViewById(R.id.etEmail);
+        etPhoneNumber = findViewById(R.id.etPhoneNumber);
+        etMessage = findViewById(R.id.etMessage);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        btnBack = findViewById(R.id.btnBack);
+
+
     }
 }
