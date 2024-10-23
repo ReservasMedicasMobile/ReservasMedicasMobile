f01fe7f3fe49ba7495f34e89bf22212c4b36c9b6
Florencia Maldonado <flor_maldonado13@hotmail.com>
2024-10-23 00:48:20 -0300

    Accesibilidad Registro
    Cambios

--- app/src/main/java/com/example/reservasmedicasmobile/registro.java
+++ app/src/main/java/com/example/reservasmedicasmobile/registro.java
@@ -15,6 +15,8 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 import com.android.volley.AuthFailureError;
@@ -47,6 +49,9 @@ public class registro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setVisibility(View.GONE);
 
         // Inicializar vistas
         usernameInput = findViewById(R.id.username_input);
@@ -103,11 +108,11 @@ public class registro extends AppCompatActivity {
                 startActivity(intent);
                 return true;
             } else if (itemId == R.id.navigation_turnos) {
-                Intent intent = new Intent(registro.this, turnos.class);
+                Intent intent = new Intent(registro.this, login.class);
                 startActivity(intent);
                 return true;
             } else if (itemId == R.id.navigation_perfil) {
-                Intent intent = new Intent(registro.this, dashboard.class);
+                Intent intent = new Intent(registro.this, login.class);
                 startActivity(intent);
                 return true;
             } else {
@@ -125,10 +130,6 @@ public class registro extends AppCompatActivity {
             Intent intent = new Intent(registro.this, login.class);
             startActivity(intent);
         });
-
-
-
-
 
     }
 
