70f97bbcd3bb80d3c70ab7d1c0c3f5404ec1ac51
Florencia Maldonado <flor_maldonado13@hotmail.com>
2024-10-23 17:33:12 -0300

    Cambios Pacientes
    Modificación accesibilidad

--- app/src/main/java/com/example/reservasmedicasmobile/pacientes.java
+++ app/src/main/java/com/example/reservasmedicasmobile/pacientes.java
@@ -62,12 +62,6 @@ public class pacientes extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_pacientes);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
 
 
         SharedPreferences sharedPreferences = getSharedPreferences("app_prefs", MODE_PRIVATE);
