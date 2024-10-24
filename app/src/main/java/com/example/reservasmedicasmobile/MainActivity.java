c6285277276a1daddf5044c4cb354c9ddf10f3ab
jonjonathanarias <106196489+jonjonathanarias@users.noreply.github.com>
2024-10-24 00:17:01 -0300

    actualizacion

--- app/src/main/java/com/example/reservasmedicasmobile/MainActivity.java
+++ app/src/main/java/com/example/reservasmedicasmobile/MainActivity.java
@@ -21,10 +21,90 @@ public class MainActivity extends AppCompatActivity {
 
         // Setup Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
+<<<<<<< Updated upstream
         setSupportActionBar(toolbar);
+=======
+        toolbar.setVisibility(View.GONE);
+
+        // Inicializar botones
+        btnContacto = findViewById(R.id.btn_contacto);
+        btnServicios = findViewById(R.id.btn_servicios);
+        btnTurnos = findViewById(R.id.btn_turnos);
+        btnRegistro = findViewById(R.id.btn_registro);
+        btnPerfil = findViewById(R.id.btn_dashboard);
+        btnLogin = findViewById(R.id.btn_login);
+        btnLogout = findViewById(R.id.btn_logout);
+
+        btnEpecialidades = findViewById(R.id.btn_especialidad);
+
+        // Inicializar el TextView para el mensaje de saludo
+        TextView mensajeSaludo = findViewById(R.id.mensaje_saludo);
+
+        // Comprobar si el usuario está logueado
+        SharedPreferences sharedPreferences = getSharedPreferences("app_prefs", MODE_PRIVATE);
+        boolean isLoggedIn = sharedPreferences.getBoolean("is_logged_in", false);
+        String first_name = sharedPreferences.getString("first_name", "Usuario");
+        int id = sharedPreferences.getInt("id", -1);
+
+
+
+        // Mostrar u ocultar botones según el estado de logueo
         {
             if(isLoggedIn && id == 53){
                 String saludo = getString(R.string.saludo, first_name);
+                mensajeSaludo.setText(saludo);
+                btnLogout.setVisibility(View.VISIBLE);    // Solo para usuarios logueados
+                mensajeSaludo.setVisibility(View.VISIBLE); // Ocultar mensaje si no está logueado
+                btnEpecialidades.setVisibility(View.VISIBLE);
+                btnContacto.setVisibility(View.GONE);
+                btnServicios.setVisibility(View.GONE);
+                btnRegistro.setVisibility(View.GONE);
+                btnPerfil.setVisibility(View.GONE);
+                btnLogin.setVisibility(View.GONE);
+                btnTurnos.setVisibility(View.GONE);
+            }else if (isLoggedIn && id !=53) {
+
+                // Usar el string resource para el mensaje de saludo
+                String saludo = getString(R.string.saludo, first_name);
+                mensajeSaludo.setText(saludo); // Configurar el mensaje de saludo
+
+                btnServicios.setVisibility(View.GONE); // Solo para usuarios no logueados
+                btnRegistro.setVisibility(View.GONE);  // Solo para usuarios no logueados
+                btnLogin.setVisibility(View.GONE);     // Solo para usuarios no logueados
+
+
+
+
+
+
+            } else {
+                btnTurnos.setVisibility(View.GONE);    // Solo para usuarios logueados
+                btnPerfil.setVisibility(View.GONE);    // Solo para usuarios logueados
+                btnLogout.setVisibility(View.GONE);    // Solo para usuarios logueados
+                mensajeSaludo.setVisibility(View.GONE); // Ocultar mensaje si no está logueado
+
+            }
+
+
+        }
+
+        // Asignar eventos a los botones
+        btnContacto.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, contacto.class)));
+        btnServicios.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, servicios.class)));
+        btnTurnos.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, turnos.class)));
+        btnRegistro.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, registro.class)));
+        btnPerfil.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, dashboard.class)));
+        btnLogin.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, login.class)));
+
+        btnEpecialidades.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, especialidades.class)));
+
+        btnLogout.setOnClickListener(v -> {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean("is_logged_in", false);
+            editor.apply();
+            recreate(); // Refresca la actividad para aplicar los cambios
+        });
+>>>>>>> Stashed changes
     }
 
     @Override
