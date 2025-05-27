99682a3db54b27c89427ec0adb7b3b3618dc6135
Florencia Maldonado <flor_maldonado13@hotmail.com>
2025-05-26 21:40:49 -0300

    Cambios en registro y Mis turnos
    Modificaciones funcionales y estéticas

--- app/src/main/java/com/example/reservasmedicasmobile/registro.java
+++ app/src/main/java/com/example/reservasmedicasmobile/registro.java
@@ -15,23 +15,19 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.DefaultRetryPolicy;
-import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import android.util.Log;  // Importar la clase Log
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
 import java.util.Map;
 import android.text.Editable;
 import android.text.TextWatcher;
-
 
 public class registro extends AppCompatActivity {
 
@@ -40,9 +36,11 @@ public class registro extends AppCompatActivity {
     private EditText last_nameInput;
     private EditText emailInput;
     private EditText passwordInput;
+    private EditText confirmPasswordInput;
     private Button registerBtn;
-    private ImageButton backButton; // Declaración de ImageButton
+    private ImageButton backButton;
     private TextView passwordRequirements;
+    private TextView passwordMismatchError;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -50,22 +48,20 @@ public class registro extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
-
         // Inicializar vistas
         usernameInput = findViewById(R.id.username_input);
         first_nameInput = findViewById(R.id.etFirstName);
         last_nameInput = findViewById(R.id.etLastName);
         emailInput = findViewById(R.id.email_input);
         passwordInput = findViewById(R.id.password_input);
+        confirmPasswordInput = findViewById(R.id.confirm_password_input);
         registerBtn = findViewById(R.id.inicio_btn);
-        backButton = findViewById(R.id.back_button); // Inicializar el botón
         passwordRequirements = findViewById(R.id.password_requirements);
+        passwordMismatchError = findViewById(R.id.password_mismatch_error);
 
-        // Advertencia de campos requeridos
+        // Validación dinámica de la contraseña
         passwordInput.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-
+            @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 String password = s.toString();
@@ -79,138 +75,124 @@ public class registro extends AppCompatActivity {
                     passwordRequirements.setVisibility(View.VISIBLE);
                 }
             }
-
-            @Override
-            public void afterTextChanged(Editable s) {}
+            @Override public void afterTextChanged(Editable s) {}
         });
 
-
-        // Configurar el botón de registro
-        registerBtn.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                System.out.println("presione registro");
-                // Validar campos y registrar usuario
-                registerUser();
-            }
+        registerBtn.setOnClickListener(v -> {
+            Log.d("Registro", "Presionó registro");
+            registerUser();
         });
 
-        // Configurar BottomNavigationView
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
             int itemId = item.getItemId();
 
             if (itemId == R.id.navigation_home) {
-                Intent intent = new Intent(registro.this, MainActivity.class);
-                startActivity(intent);
+                startActivity(new Intent(registro.this, MainActivity.class));
                 return true;
             } else if (itemId == R.id.navigation_login) {
-                // Navegar a TurnosActivity
-                Intent intent = new Intent(registro.this, login.class);
-                startActivity(intent);
+                startActivity(new Intent(registro.this, login.class));
                 return true;
             } else if (itemId == R.id.navigation_servicios) {
-                Intent intent = new Intent(registro.this, servicios.class);
-                startActivity(intent);
+                startActivity(new Intent(registro.this, servicios.class));
                 return true;
             } else {
                 return false;
             }
         });
 
-        // Establecer filtros de entrada para el DNI
+        // Limitar longitud de DNI
         usernameInput.setFilters(new InputFilter[]{new InputFilter.LengthFilter(8)});
 
-
-        // Link a login
+        // Link para ir a login
         TextView login1 = findViewById(R.id.login1);
-        login1.setOnClickListener(v -> {
-            Intent intent = new Intent(registro.this, login.class);
-            startActivity(intent);
-        });
-
-
-
-
-
+        login1.setOnClickListener(v -> startActivity(new Intent(registro.this, login.class)));
     }
 
     public void VolverInicio(View view) {
-        Intent volverInicio = new Intent(registro.this, MainActivity.class);
-        startActivity(volverInicio);
+        startActivity(new Intent(registro.this, MainActivity.class));
     }
 
     private void registerUser() {
-        System.out.println("entre a la funcion registro");
+        Log.d("Registro", "Inicio de registro");
         String username = usernameInput.getText().toString().trim();
         String first_name = first_nameInput.getText().toString().trim();
         String last_name = last_nameInput.getText().toString().trim();
         String email = emailInput.getText().toString().trim();
         String password = passwordInput.getText().toString().trim();
+        String confirmPassword = confirmPasswordInput.getText().toString().trim();
 
-        // Validar DNI (username)
+        // Validaciones
         if (TextUtils.isEmpty(username)) {
             usernameInput.setError("El DNI es obligatorio");
             return;
-        } else if (!username.matches("\\d+")) { // Verifica que sea numérico
+        } else if (!username.matches("\\d+")) {
             usernameInput.setError("El DNI debe ser numérico");
             return;
-        } else if (username.length() != 8) { // Verifica que tenga exactamente 8 dígitos
+        } else if (username.length() != 8) {
             usernameInput.setError("El DNI debe tener exactamente 8 dígitos");
             return;
-        } else if (username.startsWith("0")) {  // El dni no puede empezar con 0
+        } else if (username.startsWith("0")) {
             usernameInput.setError("El DNI no puede empezar con '0'");
             return;
-        } else if (username.matches("(\\d)\\1{7}")) { // El dni no podra tener 8 números repetidos (ej. "11111111")
+        } else if (username.matches("(\\d)\\1{7}")) {
             usernameInput.setError("El DNI no puede contener 8 números repetidos");
             return;
         }
 
-        // Validar nombre
         if (TextUtils.isEmpty(first_name)) {
             first_nameInput.setError("El nombre es obligatorio");
             return;
-        } else if (!first_name.matches("[A-Za-z ]+")) {  // Permitimos espacios
+        } else if (!first_name.matches("[A-Za-z ]+")) {
             first_nameInput.setError("El nombre no puede contener caracteres especiales");
             return;
-        }  else if (first_name.length() < 3 || first_name.length() > 20) {  // el nombre debe tener un minimo de 3 caracteres y un maximo de 20
+        } else if (first_name.length() < 3 || first_name.length() > 20) {
             first_nameInput.setError("El nombre es demasiado corto");
             return;
         }
 
-        // Validar apellido
         if (TextUtils.isEmpty(last_name)) {
             last_nameInput.setError("El apellido es obligatorio");
             return;
-        } else if (!last_name.matches("[A-Za-z ]+")) {  // Permitimos espacios
+        } else if (!last_name.matches("[A-Za-z ]+")) {
             last_nameInput.setError("El apellido no puede contener caracteres especiales");
             return;
-        } else if (last_name.length() < 2 || last_name.length() > 20) {  // Validación de longitud
+        } else if (last_name.length() < 2 || last_name.length() > 20) {
             last_nameInput.setError("El apellido es demasiado corto");
             return;
         }
 
-        // Validar correo electrónico
         if (TextUtils.isEmpty(email)) {
             emailInput.setError("El correo electrónico es obligatorio");
             return;
-        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) { // Verifica formato de correo electrónico
-            emailInput.setError("Correo electrónico inválido, debe seguir el formato de un correo valido ej:nombre22@gmail.com");
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailInput.setError("Correo electrónico inválido, debe seguir el formato de un correo válido ej: nombre22@gmail.com");
             return;
         }
 
-        // Validar contraseñas
         if (TextUtils.isEmpty(password)) {
-            passwordInput.setError("La contraseña es obligatoria");
+            passwordRequirements.setText("La contraseña es obligatoria");
+            passwordRequirements.setVisibility(View.VISIBLE);
+            passwordMismatchError.setVisibility(View.GONE);
             return;
-        } else if (!isPasswordValid(password)) { // Verifica que la contraseña cumpla con los requisitos
-            passwordInput.setError("La contraseña debe tener entre 8 y 16 caracteres, e incluir una mayuscula, números y símbolos");
+        } else if (!isPasswordValid(password)) {
+            passwordRequirements.setText("La contraseña debe tener entre 8 y 16 caracteres, e incluir una mayúscula, números y símbolos");
+            passwordRequirements.setVisibility(View.VISIBLE);
+            passwordMismatchError.setVisibility(View.GONE);
             return;
+        } else {
+            passwordRequirements.setVisibility(View.GONE);
         }
 
-        System.out.println("pase la validacion ");
+        if (!password.equals(confirmPassword)) {
+            passwordMismatchError.setVisibility(View.VISIBLE);
+            return;
+        } else {
+            passwordMismatchError.setVisibility(View.GONE);
+        }
+
+        Log.d("Registro", "Validación superada");
 
         String url = "https://reservasmedicas.ddns.net/register/";
 
@@ -224,54 +206,41 @@ public class registro extends AppCompatActivity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        String jsonString = jsonBody.toString();
-        Log.d("JSON Request", jsonString);  // Imprime el JSON en los logs
+
+        Log.d("JSON Request", jsonBody.toString());
 
         int timeoutMs = 10000;
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, jsonBody,
                 response -> {
-                    // Código 200 o 201 - Registro exitoso
                     try {
-                        // Imprimir el JSON de respuesta en la consola
                         Log.d("RegistroResponse", response.toString());
-
-                        // Extraer el token del JSON de respuesta
                         String token = response.getString("token");
 
-                        // Guardar el token en SharedPreferences
                         SharedPreferences prefs = getSharedPreferences("app_prefs", MODE_PRIVATE);
                         SharedPreferences.Editor editor = prefs.edit();
                         editor.putString("auth_token", token);
                         editor.putString("first_name", first_name);
-                        editor.putBoolean("is_logged_in", true);// alejo
-
+                        editor.putBoolean("is_logged_in", true);
                         editor.apply();
 
-                        // Mostrar mensaje de éxito y limpiar el formulario
                         Toast.makeText(this, "Usuario registrado con éxito, ya puede iniciar sesión", Toast.LENGTH_SHORT).show();
                         clearForm();
 
-                        // Volver al inicio (MainActivity)
-                        Intent intent = new Intent(registro.this, login.class);
-                        startActivity(intent);
+                        startActivity(new Intent(registro.this, login.class));
                         finish();
 
                     } catch (JSONException e) {
-                        // Manejo del error de JSON
                         e.printStackTrace();
                         Toast.makeText(this, "Error procesando la respuesta del servidor", Toast.LENGTH_SHORT).show();
                     }
                 },
                 error -> {
-                    // Manejar errores HTTP
                     if (error.networkResponse != null) {
                         int statusCode = error.networkResponse.statusCode;
                         if (statusCode == 400) {
-                            // Usuario ya registrado
                             Toast.makeText(this, "El usuario ya está registrado", Toast.LENGTH_SHORT).show();
                         } else if (statusCode == 404) {
-                            // Sin conexión con el backend
                             Toast.makeText(this, "Error de conexión: no se pudo contactar al servidor", Toast.LENGTH_SHORT).show();
                         } else {
                             Toast.makeText(this, "Error: " + statusCode, Toast.LENGTH_SHORT).show();
@@ -288,34 +257,28 @@ public class registro extends AppCompatActivity {
             }
         };
 
-        jsonObjectRequest.setRetryPolicy(new
-                DefaultRetryPolicy(
+        jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(
                 timeoutMs,
                 DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
                 DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
         ));
 
-        VolleySingleton.getInstance(this).
-                addToRequestQueue(jsonObjectRequest);
+        VolleySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
     }
 
-
-
-    // Limpiar el formulario después de un registro exitoso
     private void clearForm() {
         usernameInput.setText("");
         first_nameInput.setText("");
         last_nameInput.setText("");
         emailInput.setText("");
         passwordInput.setText("");
+        confirmPasswordInput.setText("");
     }
 
-
     private boolean isPasswordValid(String password) {
-        // La contraseña debe tener entre 8 y 16 caracteres, e incluir al menos un número, un símbolo y una letra mayúscula
         return password.length() >= 8 && password.length() <= 16
-                && password.matches(".*[0-9].*") // Al menos un número
-                && password.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\|,.<>\\/?].*") // Al menos un símbolo
-                && password.matches(".*[A-Z].*"); // Al menos una letra mayúscula
+                && password.matches(".*[0-9].*")
+                && password.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?].*")
+                && password.matches(".*[A-Z].*");
     }
 }
